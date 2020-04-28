86d3056d613b07c1f33aab36d00a84ffa9f44811
HapLeo <hapleow@gmail.com>
2020-04-28 15:53:30 +0800

    init

---
+++ src/main/java/com/homebox/mysql_structure/MysqlStructureApplication.java
@@ -0,0 +1,13 @@
+package com.homebox.mysql_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homebox/mysql_structure/MysqlStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homebox.mysql_structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
